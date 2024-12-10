7d1168663e0e0e8617a293b1410be74453667317
Ana Barbosa <anael.noki@gmail.com>
2024-12-10 15:31:24 -0300

    feat: adiciona views e urls

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("municipio/", views.municipio, name="municipio"),
+    path("ouvidoria/", views.ouvidoria, name="ouvidoria"),
+    path("esic/", views.esic, name="esic"),
+    path("ex-presidentes/", views.ex_presidentes, name="ex-presidentes"),
+    path("projetos-de-lei/", views.projetos_de_lei, name="projetos-de-lei"),
 ]
--- app/views.py
+++ app/views.py
@@ -5,3 +5,28 @@ from django.shortcuts import render
 def index(request):
     template = "index.html"
     return render(request, template)
+
+
+def municipio(request):
+    template = "app/municipio.html"
+    return render(request, template)
+
+
+def ouvidoria(request):
+    template = "app/ouvidoria.html"
+    return render(request, template)
+
+
+def esic(request):
+    template = "app/esic.html"
+    return render(request, template)
+
+
+def ex_presidentes(request):
+    template = "app/ex-presidentes.html"
+    return render(request, template)
+
+
+def projetos_de_lei(request):
+    template = "app/projetos-de-lei.html"
+    return render(request, template)
