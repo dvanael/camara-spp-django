52561033602126717804d22b0a9709b5b0b5fe71
Ana Barbosa <anael.noki@gmail.com>
2024-12-12 07:49:07 -0300

    feat: atualiza urls e adiciona nova view

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from . import views
+from .views import index, municipio, ouvidoria, esic, ex_presidentes, ProjetosDeLeiView
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("municipio/", views.municipio, name="municipio"),
-    path("ouvidoria/", views.ouvidoria, name="ouvidoria"),
-    path("esic/", views.esic, name="esic"),
-    path("ex-presidentes/", views.ex_presidentes, name="ex-presidentes"),
-    path("projetos-de-lei/", views.projetos_de_lei, name="projetos-de-lei"),
+    path("", index, name="index"),
+    path("municipio/", municipio, name="municipio"),
+    path("ouvidoria/", ouvidoria, name="ouvidoria"),
+    path("esic/", esic, name="esic"),
+    path("ex-presidentes/", ex_presidentes, name="ex-presidentes"),
+    path("projetos-de-lei/", ProjetosDeLeiView.as_view(), name="projetos-de-lei"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,6 @@
+from django.views.generic import ListView
 from django.shortcuts import render
+from .models import ProjetoDeLei
 
 
 # Create your views here.
@@ -27,6 +29,30 @@ def ex_presidentes(request):
     return render(request, template)
 
 
-def projetos_de_lei(request):
-    template = "app/projetos-de-lei.html"
-    return render(request, template)
+class ProjetosDeLeiView(ListView):
+    model = ProjetoDeLei
+    template_name = "app/projetos-de-lei.html"
+    context_object_name = "object_list"
+    paginate_by = 2
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search = self.request.GET.get("search")
+        ano = self.request.GET.get("ano")
+
+        if search:
+            queryset = queryset.filter(numero__icontains=search) | queryset.filter(
+                descricao__icontains=search
+            )
+
+        if ano:
+            queryset = queryset.filter(ano=int(ano))
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["anos"] = ["2021", "2022", "2023", "2024"]
+        context["search"] = self.request.GET.get("search", "")
+        context["ano_selected"] = self.request.GET.get("ano")
+        return context
