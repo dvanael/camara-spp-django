4b39e5ab160d1f7509d0bb9d271f803d29bef9f7
dvanael <anael.noki@gmail.com>
2024-12-12 09:09:04 -0300

    feat: adiciona crispy forms

--- config/settings.py
+++ config/settings.py
@@ -26,10 +26,15 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # 3rd Party
+    "crispy_forms",
+    "crispy_bootstrap5",
     # Local Apps
     "app",
 ]
 
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
